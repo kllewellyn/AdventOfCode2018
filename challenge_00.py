0aa7c8b0058b11a1d48dc2ea2d18d020dea9b6c1
Your Name <you@example.com>
2018-12-02 23:21:38 -0500

    Challenge 0 part 1

---
+++ challenge_00.py
@@ -0,0 +1,22 @@
+import sys
+
+count = 0
+
+def add(addNumber):
+    global count
+    count = count + int(addNumber)
+
+def subtract(number):
+    global count
+    count = count - int(number)
+
+def main():
+    for line in sys.stdin:
+        if (line[0] == '+'):
+            add(line[1:])
+        else:
+            subtract(line[1:])
+    print(count)
+
+if __name__== "__main__":
+  main()
